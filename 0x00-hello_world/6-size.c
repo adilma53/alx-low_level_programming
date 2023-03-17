8f319bc0131e708995bb1c1bcec7b0c52b9056a6
Adil Mae <itsadilhadid@gmail.com>
2023-03-17 02:38:27 +0100

    test

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - i have a simple function put that prints a string.
+  *
+  * Return: if success return 0.
+  */
+int main(void)
+{
+  printf("Size of a char: %d byte(s)\n", sizeof(char));
+  printf("Size of an int: %d byte(s)\n", sizeof(int));
+  printf("Size of a long int: %d byte(s)\n", sizeof(long));
+  printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+  printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
+
