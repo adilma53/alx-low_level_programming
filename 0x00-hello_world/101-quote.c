66e27f1cc863e5a2a140c6fc3580ebc5debfeb9e
Adil Mae <itsadilhadid@gmail.com>
2023-03-17 03:07:33 +0100

    test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
 /**
   * main - i have a simple function put that prints a string.
   *
-  * Return: if success return 0.
+  * Return: if success return 1.
   */
 int main(void)
-{
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, quote, sizeof(quote) - 1);
-	return 0;
+}
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return 1;
 }
