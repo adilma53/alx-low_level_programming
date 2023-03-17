878c8246188e3b3ca6e1046dd112f55c061e0a66
Adil Mae <itsadilhadid@gmail.com>
2023-03-17 02:14:37 +0100

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 /**
   * main - i have a simple function put that prints a string.
-  */
+  * Return - if success return 0.
+  * /
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
