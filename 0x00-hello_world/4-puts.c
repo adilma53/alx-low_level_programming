78ea6e83ee0c6ca03419b4be59e3ddb23c10764b
Adil Mae <itsadilhadid@gmail.com>
2023-03-17 01:17:18 +0100

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,14 @@
 
 int main(void)
 {
+
+/**
+ * 
+ *	i used an array here.
+ * 
+ * 
+ */
 	char greeting[] = "\"Programming is like building a multilingual puzzle";
 	puts(greeting);
-    
-	return 0;
+	return (0);
 }
