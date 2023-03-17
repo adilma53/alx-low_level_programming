71d45ed9f2758179febccb645e3d2c567d80f0ee
Adil Mae <itsadilhadid@gmail.com>
2023-03-17 23:56:45 +0100

    alx

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - print all alphabets
+  *
+  * Return: 0 if success
+  */
+
+int main(void)
+{
+char str[] = "abcdefghijklmnopqrstuvwxyz";
+int i = 0;
+
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+return (0);
+}
