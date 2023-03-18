4d093ace980b51b3501e2fdb73d9994bb885d137
Adil Mae <itsadilhadid@gmail.com>
2023-03-18 01:42:33 +0100

    alx

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - print all alphabets upper and lowecase
+  *
+  * Return: 0 if success
+  */
+
+int main(void)
+{
+char str[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i = 0;
+
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+
+putchar('\n')
+
+return (0);
+}
