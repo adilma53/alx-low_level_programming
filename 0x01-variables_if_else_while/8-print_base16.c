833ca7efb2bc2cd6770e08bdbfdf3ed8f799f35f
Adil Mae <itsadilhadid@gmail.com>
2023-03-18 03:36:08 +0100

    alx

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * main - print all alphabets upper and lowecase
+  *
+  * Return: 0 if success
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	int c;
+
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
