f3824612a202b4a85e8390fdd19b45568ec83ca0
Adil Mae <itsadilhadid@gmail.com>
2023-03-18 02:31:52 +0100

    alx

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - print all alphabets upper and lowecase
+  *
+  * Return: 0 if success
+  */
+
+int main(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	int size = sizeof(str) / sizeof(str[0]);
+
+
+	for (int i = size - 1; i >= 0 ; i--)
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
