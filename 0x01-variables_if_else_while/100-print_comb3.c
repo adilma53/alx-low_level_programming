8a0205072828f97e68a4ec9c43ee1b2020b83987
Adil Mae <itsadilhadid@gmail.com>
2023-03-20 23:53:49 +0000

    alx

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print all two-digit combinations without repeating digits.
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i, tens_digit, ones_digit;
+
+	for (i = 0; i < 100; i++)
+	{
+		tens_digit = i / 10;
+		ones_digit = i % 10;
+
+		if (tens_digit >= ones_digit)
+			continue;
+
+
+		putchar(tens_digit + '0');
+		putchar(ones_digit + '0');
+
+		if (i < 89)
+			printf(", ");
+	}
+
+	putchar('\n');
+
+}
