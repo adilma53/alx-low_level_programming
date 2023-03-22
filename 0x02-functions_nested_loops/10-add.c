ff7743686261f874f4974d263b3b1e851ee9f9ed
Adil Mae <itsadilhadid@gmail.com>
2023-03-22 02:44:24 +0000

    alx

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+* add - return sum
+*
+* Return: 0
+*/
+
+int add(int a, int b)
+{
+
+
+
+        sum = a + b;
+        if (sum > 9)
+
+        {
+                return (_putchar((sum / 10) + '0'));
+                return (_putchar((sum % 10) + '0'));
+        }
+
+        else
+        {
+                return (_putchar(sum + '0'));
+        }
+
+
+}
