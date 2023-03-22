b4f88ca1edfcec4539fea6e69e0e680d91341805
Adil Mae <itsadilhadid@gmail.com>
2023-03-22 01:39:13 +0000

    alx

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,8 +13,8 @@ void jack_bauer(void)
         int j;
         int i;
 
-        for (j = 0; j < 24; j++;)
-                for (i = 0; i < 60; i++;)
+        for (j = 0; j < 24; j++)
+                for (i = 0; i < 60; i++)
                 {
                         _putchar(j / 10 + '0');
                         _putchar(j % 10 + '0');
