4769dc7348d9496b529bcde50b30fe508b26bd76
Adil Mae <itsadilhadid@gmail.com>
2023-03-22 00:06:09 +0000

    alx

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Returns absolute number
+ * @n: input.
+ * Return: Returns absolute number.
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		unsigned int positive = n;
+
+		return (positive);
+
+	}
+}
