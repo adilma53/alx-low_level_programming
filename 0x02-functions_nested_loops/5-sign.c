5350c496e748d4915ec43f4bdd2b67a47d4cfa33
Adil Mae <itsadilhadid@gmail.com>
2023-03-22 02:58:11 +0000

    alx

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,25 @@
 #include "main.h"
-
 /**
  * print_sign - checks if n value.
  * @n: input.
  * Return: Returns 1 if n is posotove 0, if n=0 , Returns -1 if n negative.
  */
 
+
 int print_sign(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else if (n > 0)
-	{
-		_putchar('+');
-		return (-1);
-	}
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+else
+{
+_putchar('-');
+return (-1);
+}
 }
