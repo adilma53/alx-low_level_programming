d782fcbe2f1b784b3cf5bda451df1136aee8ea19
Adil Mae <itsadilhadid@gmail.com>
2023-03-22 02:38:04 +0000

    alx

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,33 +1,40 @@
 #include "main.h"
 
+
 /**
-* time_teble - prints multiplication table
+* times_table - prints multiplication table
 *
 * Return: nada
 */
 
-void time_teble(void)
+void times_table(void)
 {
     int j, i, product;
 
     for (j = 0; j <= 9; j++)
     {
-        _putchar('0');
-
-        for (i = 1; i <= 9; i++)
+        for (i = 0; i <= 9; i++)
         {
+
+            product = j * i;
+            if (product > 9)
+
+            {
+                _putchar((product / 10) + '0');
+                _putchar((product % 10) + '0');
+            }
+
+            else
+            {
+                _putchar(product + '0');
+            }
+
             _putchar(',');
             _putchar(' ');
 
-            product = j * i;
-
-            if (product <= 9)
-                _putchar(' ');
-            else
-                _putchar((product / 10) + '0');
-
-            _putchar((product % 10) + '0');
         }
         _putchar('\n');
+
     }
+
 }
