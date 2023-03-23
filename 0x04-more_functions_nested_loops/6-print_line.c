df0f31206b475a361ee47c5ac7076253c293f695
Adil Mae <itsadilhadid@gmail.com>
2023-03-23 22:01:00 +0000

    alx

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_line - prints multiple _
+* @n: input.
+* Return:returns nothing
+*/
+
+void print_line(int n)
+{
+
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
