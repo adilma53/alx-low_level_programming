08e6e1e710d58b012af09143e441353d2139e125
Adil Mae <itsadilhadid@gmail.com>
2023-03-23 21:05:43 +0000

    alx

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9.
+ *
+ *
+ * Return: nothing.
+ */
+
+void print_numbers(void)
+{
+for (int i = 0; i < 10; ++i)
+{
+putchar(i + '0');
+}
+putchar('\n');
+}
