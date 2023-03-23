114d4dbf2780fcca8980862a356a91dc2708e9a3
Adil Mae <itsadilhadid@gmail.com>
2023-03-23 22:24:15 +0000

    alx

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,21 @@
 #include "main.h"
-
 /**
- *print_numbers - print from 0 to 9 avoid 2 and 4.
- *
- *Return: nothing.
- */
-
-void print_numbers(void)
+*print_most_numbers - prints numbers from 0 to 9 skippin
+* 2 an 4
+*
+*Return: nothing
+*/
+void print_most_numbers(void)
 {
 
 int i;
 
-for (i = 0; i < 10; ++i)
+for (i = 0; i < 10; i++)
 {
-if (i != 2 && i != 4)
+if ((i != 2) && (i != 4))
 {
 _putchar(i + '0');
 }
-
 }
 _putchar('\n');
 }
