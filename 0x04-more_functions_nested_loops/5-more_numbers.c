244df17848b7bd17c8492c40c5a30f6541d3c910
Adil Mae <itsadilhadid@gmail.com>
2023-03-23 21:20:17 +0000

    alx

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,14 @@ void more_numbers(void)
 {
 
 int i;
+int j;
 
 for (i = 0; i < 10; ++i)
 {
 
-for (int i = 0; i < 15; ++i)
+for (int j = 0; j < 15; ++j)
 {
-_putchar(i + '0');
+_putchar(j + '0');
 
 }
 _putchar('\n');
