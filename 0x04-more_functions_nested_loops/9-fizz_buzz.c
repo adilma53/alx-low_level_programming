809415da98115f12bafb132c32747f35f87addd7
Adil Mae <itsadilhadid@gmail.com>
2023-03-24 02:23:53 +0000

    alx

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
 *main - program that prints either number
-*or fizz or buzz or fizzBuzz
+*or fizz or buzz or fizzBuzz.
 *
-*
-*Return: returns 0
+*Return: returns 0.
 */
 int main(void)
 {
+
 int i;
 
 for (i = 1; i <= 100; i++)
@@ -32,9 +33,7 @@ else
 {
 printf("%d", i);
 }
-
 }
-
 printf("\n");
 return (0);
 }
