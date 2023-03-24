3797bf1a78cff71f14c23026e352959fcd1995eb
Adil Mae <itsadilhadid@gmail.com>
2023-03-24 01:45:43 +0000

    alx

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,6 @@
-#include <stdio.h>
+int i;
 
-int main()
-{
-
-for (int i = 1; i <= 100; i++)
+for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0)
 {
@@ -31,6 +28,4 @@ putchar(i + '0');
 }
 }
 putchar(' ');
-}
-
 }
