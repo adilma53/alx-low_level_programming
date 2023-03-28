b1adf5003934342de24aa07ae905516dc28ad2f1
Adil Mae <itsadilhadid@gmail.com>
2023-03-28 18:23:14 +0000

    alx

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,31 +2,28 @@
 
 /**
 * puts_half - print half of a string
-*  if string length not od prints length+1
 * @str: string to be printed
 */
 
 void puts_half(char *str)
 {
+int length=0, half;
 
-int length = 0;
-int half;
-int i;
 
 while (str[length] != '\0')
 {
 length++;
 }
 
+half = length / 2;
 
-if (length %2 != 0)
-half = (length / 2) - 1;
-else
-half = (length / 2);
+if (length % 2 != 0)
+half++;
 
-for (i = half; i < length; i++)
+while (half < length)
 {
-_putchar(str[i]);
+_putchar(str[half]);
+half++;
 }
 _putchar('\n');
 }
