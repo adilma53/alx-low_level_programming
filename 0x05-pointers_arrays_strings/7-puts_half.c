79dd32584c881b73ce280ddd70bd57ef3a07772c
Adil Mae <itsadilhadid@gmail.com>
2023-03-28 17:45:30 +0000

    alx

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* print_half - print half of a string
+*  if string length not od prints length+1
+* @str: string to be printed
+*/
+
+void print_half(char *str) {
+
+int length = 0;
+int half;
+int i;
+
+while (str[length] != '\0') {
+length++;
+}
+
+if (length % 2 != 0)
+half = (length / 2) + 1;
+
+else
+half = (length / 2);
+
+for ( i = 0; i < half; i++) {
+_putchar(str[i]);
+}
+_putchar('\n');
+}
