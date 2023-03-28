bcda1fa12b3811fcb9dcdd7627f11f17323b6c44
Adil Mae <itsadilhadid@gmail.com>
2023-03-28 00:26:51 +0000

    alx

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+*print_rev - function that prints a string i nreverse
+*@s : string
+*Return: nothing
+*/
+
+
+void print_rev(char *s);
+{
+
+int i;
+int size;
+
+while (s[size] != '\0')
+{
+size++;
+}
+
+
+for (i = size - 1 ; i >= 0 ; i--)
+{
+
+_putchar(s[i]);
+
+}
+
+_putchar(10);
+
+}
