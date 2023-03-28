4be194a4b571e030f04d57859261d5950250a1d6
Adil Mae <itsadilhadid@gmail.com>
2023-03-28 18:00:22 +0000

    alx

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -22,15 +22,16 @@ length++;
 if (length % 2 != 0)
 {
 half = (length / 2) + 1;
-for (i = half - 1 ; i < length; i++) {
+for (i = half - 1 ; i < length; i++)
+{
 _putchar(str[i]);
 }
 }
-
 else
 {
 half = (length / 2);
-for (i = half ; i < length; i++) {
+for (i = half ; i < length; i++)
+{
 _putchar(str[i]);
 }
 }
