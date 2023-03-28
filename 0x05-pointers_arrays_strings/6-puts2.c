a4f0029fa9c83c2401b04f27cbcded4a5190daf1
Adil Mae <itsadilhadid@gmail.com>
2023-03-28 16:04:39 +0000

    alx

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* puts2 - print every charachter of string
+* @str: string to be printed
+*/
+
+void puts2(char *str)
+{
+
+int j=0;
+
+while (s[j] != '\0')
+{
+_putachar(s[j]);
+_putachar('\n');
+j++;
+}
+
+}
