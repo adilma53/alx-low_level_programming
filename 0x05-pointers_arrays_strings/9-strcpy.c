0ff0c46c89337bcb09025e6f7fb3ab20715a859e
Adil Mae <itsadilhadid@gmail.com>
2023-03-29 00:31:30 +0000

    alx

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,19 +2,22 @@
 
 /**
 * _strcpy -  that copies the string to another distination
-* @dest: source
-* @src: distination
+* @dest: distination
+* @src: source
+* return: destination
 */
 
 char *_strcpy(char *dest, char *src)
 {
 int i = 0;
 
-while (src[i])
+while (src[i] != '\0')
 {
 dest[i] = src[i];
 i++;
 }
+
+dest[i] = '\0';
 
 return (dest);
 }
