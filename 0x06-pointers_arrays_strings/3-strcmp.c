ec367f5987825219425c156976e1993ca46dd1e8
adil mae <itsadilhadid@gmail.com>
2023-03-30 23:23:37 +0000

    alx

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
-* _strcmp - function that compares two strings.
+* _strcmp -  function that compares two strings.
 * @s1: string 1
-* @s2: string 2
+* @s2: string 1
 *
 * Return: s1[i] - s2[i]
 */
+
 int _strcmp(char *s1, char *s2)
 {
 
@@ -16,7 +17,6 @@ for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 {
 if (s1[i] != s2[i])
 return (s1[i] - s2[i]);
-
 }
-
+return (0);
 }
