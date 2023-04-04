a0c8f76f3eb64c22da7092e17aa0b17fce1a6cc9
adil mae <itsadilhadid@gmail.com>
2023-04-04 00:33:21 +0000

    alx

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_chessboard - a function that searches
+* a string for any of a set of bytes.
+*
+* @a: input
+*
+* Return: nothing
+*/
+
+void print_chessboard(char (*a)[8])
+{
+
+int i = 0;
+int j = 0;
+
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+putchar(a[i][j]);
+}
+putchar('\n');
+}
+
+}
