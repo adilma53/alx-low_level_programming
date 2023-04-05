d4f76907e3be2f36f31cab2dfdc29231ebf2102f
adil mae <itsadilhadid@gmail.com>
2023-04-05 17:16:51 +0000

    alx

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen_recursion - claculate length of string
+*
+* @s: input
+*
+* Return: length of string
+*
+*/
+
+int _strlen_recursion(char *s)
+{
+
+if (*s)
+return (1 + _strlen_recursion(s + 1));
+
+}
