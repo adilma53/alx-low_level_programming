aa9469c1b8b07fd6956ddc9d9ff02bd3f222096d
adil mae <itsadilhadid@gmail.com>
2023-04-06 21:32:18 +0000

    alx

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+/** By -{adilma53}- */
+
+/**
+* _strlen_recursion - claculate length of string
+*
+* @s: input
+*
+* Return: length of string
+*
+*/
+
+int _strlen_recursion(char *s)
+{
+
+if (*s == 0)
+{
+return (0);
+}
+else
+return (1 + _strlen_recursion(s + 1));
+}
+
+/** By -{adilma53}- */
+
+/**
+* check_palindrome - checks if string is palindrome
+*
+* @s: input
+* @start: input
+* @end: input
+*
+* Return: 1 if palindrome and 0 if not.
+*/
+
+int check_palindrome(char *s, int start, int end)
+{
+if (start >= end)
+{
+return (1);
+}
+else if (s[start] != s[end])
+{
+return (0);
+}
+else
+{
+return (check_palindrome(s, start + 1, end - 1));
+}
+}
+
+/** By -{adilma53}- */
+
+/**
+* is_palindrome - checks if a string is palindrome
+*
+* @s: input
+* @start: input
+* @end: input
+*
+* Return: 1 if palindrome and 0 if not.
+*/
+
+int is_palindrome(char *s)
+{
+int length = _strlen_recursion(s);
+
+if (length <= 1)
+{
+return (1);
+}
+else
+{
+return (check_palindrome(s, 0, length - 1));
+}
+}
