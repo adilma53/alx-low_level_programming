5bc2f0918a4041a65eea72e50e550cd3972361cf
adil mae <itsadilhadid@gmail.com>
2023-04-07 16:49:32 +0000

    alx

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,16 +18,16 @@
 int main(int argc, char *argv[])
 {
 
-    if (argc == 3)
-    {
+if (argc == 3)
+{
 
-        printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-    }
-    else
-    {
-        printf("Error\n");
-        return (1);
-    }
-    return (0);
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
 
 }
