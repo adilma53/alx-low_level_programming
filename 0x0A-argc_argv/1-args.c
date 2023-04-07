e93ca449c6925b20f261fd871d50a39e3eac9604
adil mae <itsadilhadid@gmail.com>
2023-04-07 16:09:55 +0000

    alx

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/** By -{adilma53}- */
+
+/**
+* main - this function print number of arguments passed
+* passed to the program.
+*
+* @argc: argument count.
+* @argv: arguments vector.
+*
+* Return: 0
+*
+*/
+
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+
+printf("%d\n", argv);
+
+
+return (0);
+
+}
