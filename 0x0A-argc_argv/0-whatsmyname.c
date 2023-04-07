72c7ad053328e71c93c822f3af7ba5b55e6e7bc6
adil mae <itsadilhadid@gmail.com>
2023-04-07 05:41:32 +0000

    alx

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/** By -{adilma53}- */
+
+/**
+* main - this function print program name
+*
+* @argc: argument count.
+* @argv: arguments vector.
+*
+* Return: 0
+*
+*/
+
+
+int main(int argc, char *argv[])
+{
+
+printf("%s\n", argv[0]);
+
+
+return 0;
+
+}
