83933ca3df0cb8aaae0ca34bc01fc5ff23f19bcc
adil mae <itsadilhadid@gmail.com>
2023-04-08 18:50:46 +0000

    alx

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#By -{adilma53}-
+
 #turning all .c files into object files
 gcc -Wall -pedantic -Werror -Wextra -c ./*.c
 
 #adding all object files to my library
-ar -rc libmy.a ./*.o
+ar -rc liball.a ./*.o
