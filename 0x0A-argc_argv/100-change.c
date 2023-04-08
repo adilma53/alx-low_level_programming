50a333a766b46f1635163f903b6b678b9c87b2f7
adil mae <itsadilhadid@gmail.com>
2023-04-08 22:26:04 +0000

    alx

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* count_change - finding how many change coins recursively
+*
+* @change: change needed
+* @counter: dominations counnter
+*
+* Return: counter
+*
+*/
+int count_change(int change, int counter);
+
+
 /** By -{adilma53}- */
 
 /**
@@ -13,9 +25,11 @@
 * Return: success 0 / fail 1
 *
 */
+
+
+
 int main(int argc, char *argv[])
 {
-
 if (argc != 2)
 {
 printf("Error\n");
@@ -31,40 +45,47 @@ printf("0\n");
 return (0);
 }
 
-while (change > 0)
+
+else
 {
 
-
-if ( change >= 25 )
-{
-change -= 25;
-counter++;
-}
-else if (change < 25 && change >= 10)
-{
-change -= 10;
-counter++;
-}
-else if (change < 10 && change >= 5)
-{
-change -= 5;
-counter++;
-}
-else if (change < 5 && change >= 2)
-{
-change -= 2;
-counter++;
-}
-else if (change == 1)
-{
-change -= 1;
-counter++;
+count_change(change, counter);
 }
 
 
-}
 
-printf("%d\n",counter);
+
 
 return (0);
+}
+
+
+
+
+
+int count_change(int change, int counter)
+{
+
+if (change == 0)
+{
+printf("%d\n", counter);
+return (0);
+}
+
+
+if (change >= 25)
+return (count_change(change - 25, counter + 1));
+
+else if (change >= 10)
+return (count_change(change - 10, counter + 1));
+
+else if (change >= 5)
+return (count_change(change - 5, counter + 1));
+
+else if (change >= 2)
+return (count_change(change - 2, counter + 1));
+
+else if (change == 1)
+return (count_change(change - 1, counter + 1));
+
 }
