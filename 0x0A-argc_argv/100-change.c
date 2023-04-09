fe5a3c86f3caf7eb24b62f304b85a33f22d93efe
adil mae <itsadilhadid@gmail.com>
2023-04-09 03:04:26 +0000

    alx

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,33 +16,38 @@
 
 int main(int argc, char *argv[])
 {
-int counter = 0;
+
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
+
 int change = atoi(argv[1]);
+int counter = 0;
 while (change > 0)
 {
 counter++;
-
 if (change >= 25)
+{
 change -= 25;
 continue;
-
+}
 if (change < 25 && change >= 10)
+{
 change -= 10;
 continue;
-
+}
 if (change < 10 && change >= 5)
+{
 change -= 5;
 continue;
-
+}
 if (change < 5 && change >= 2)
+{
 change -= 2;
 continue;
-
+}
 if (change == 1)
 change -= 1;
 }
