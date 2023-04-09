8ddcd9426c7230c4b43de0a1dd1c0e8cc23ae498
adil mae <itsadilhadid@gmail.com>
2023-04-09 02:46:01 +0000

    alx

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** By -{adilma53}- */
+
+/**
+* main - check a program that prints the minimum number
+* of coins to make change.
+*
+* @argc: argument count.
+* @argv: arguments vector.
+*
+* Return: success 0 / fail 1
+*
+*/
+
+int main(int argc, char *argv[])
+{
+
+int change;
+int counter;
+
+if (argc != 2)
+{
+    printf("Error\n");
+    return (1);
+}
+
+change = atoi(argv[1]);
+counter = 0;
+
+while (change > 0)
+{
+    counter++;
+
+    if (change >= 25)
+    {
+        change -= 25;
+        continue;
+    }
+    if (change < 25 && change >= 10)
+    {
+        change -= 10;
+        continue;
+    }
+    if (change < 10 && change >= 5)
+    {
+        change -= 5;
+        continue;
+    }
+    if (change < 5 && change >= 2)
+    {
+        change -= 2;
+        continue;
+    }
+    if (change == 1)
+    {
+        change -= 1;
+    }
+}
+
+printf("%d\n", counter);
+return (0);
+}
