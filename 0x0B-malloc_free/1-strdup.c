9453a46b7a72376f47ce82365b11a55cc95c2bf3
adil mae <itsadilhadid@gmail.com>
2023-04-11 18:30:55 +0000

    alx

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/* by adilma53 */
+
+/**
+* _strdup - a function that coppies input
+*   string to another.
+*
+* @str: input string.
+*
+* Return: the coppy of the original string "myarr".
+*/
+
+
+char *_strdup(char *str)
+{
+	int i = 0, j;
+	char *myarr;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		i++;
+	}
+	i++;
+
+	myarr = malloc(i * sizeof(char));
+
+	for (j = 0; j < i - 1; j++)
+	{
+		myarr[j] = str[j];
+	}
+
+	myarr[i] = '\0';
+
+	return (myarr);
+}
