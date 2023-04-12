16b067c7118a7703788967492b56946c5789dddc
adil mae <itsadilhadid@gmail.com>
2023-04-12 03:38:10 +0000

    alx

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 #include "main.h"
 
-/* by adilma53 */
-
 /**
-* strtow - this function takes a string and convert it
+* strtow - this function takes a string and converts it
 *   into a 2d array of strings
 *
 * @str: string to convert
@@ -15,42 +13,41 @@
 
 char **strtow(char *str)
 {
-    int i, j, wrdcount = 0, chartotal = 0, k = 0;
-    char **matrix;
+	int i, j, wrdcount = 0, chartotal = 0, k = 0, wordlen = 0;
+	char **matrix;
 
-    if (str == NULL || *str == '\0')
-        return (NULL);
-
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if ((i == 0 || str[i - 1] == ' ') && str[i] != ' ')
-            wrdcount++;
-        chartotal++;
-    }
-    matrix = malloc((wrdcount + 1) * sizeof(char *));
-    if (matrix == NULL)
-        return (NULL);
-    /* PUTTING EVERY STRING IN ITS ARRAY TERMINATED WITH '\0' */
-    for (i = 0; i < wrdcount; i++)
-    {
-        matrix[i] = malloc(chartotal * sizeof(char));
-        if (matrix[i] == NULL)
-        {
-            /* Free allocated memory if error occurs */
-            for (j = 0; j < i; j++)
-            {
-                free(matrix[j]);
-            }
-            free(matrix);
-            return (NULL);
-        }
-        for (j = 0; str[k + i] != ' ' && str[k + i] != '\0'; j++)
-        {
-            matrix[i][j] = str[k + i];
-            k++;
-        }
-        matrix[i][j] = '\0';
-    }
-    matrix[wrdcount] = NULL;
-    return (matrix);
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((i == 0 || str[i - 1] == ' ') && str[i] != ' ')
+			wrdcount++;
+		chartotal++;
+	}
+	matrix = malloc((wrdcount + 1) * sizeof(char *));
+	if (matrix == NULL)
+		return (NULL);
+	for (i = 0; i < wrdcount; i++)
+	{
+		while (str[k] == ' ')
+			k++;
+		while (str[k + wordlen] != ' ' && str[k + wordlen] != '\0')
+			wordlen++;
+		matrix[i] = malloc((wordlen + 1) * sizeof(char));
+		if (matrix[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(matrix[j]);
+			free(matrix);
+			return (NULL);
+		}
+		for (j = 0; j < wordlen; j++)
+		{
+			matrix[i][j] = str[k];
+			k++;
+		}
+		matrix[i][j] = '\0';
+	}
+	matrix[wrdcount] = NULL;
+	return (matrix);
 }
