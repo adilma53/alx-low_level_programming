e8f676e2ade63c95a6b1b2e734432392bf4c9409
adil mae <itsadilhadid@gmail.com>
2023-04-13 14:20:12 +0000

    alx

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,6 +10,20 @@
  *
  * Return: the 2d array
  */
+
+int word_count(*str)
+{
+	int wordcount = 0, i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((i == 0 || str[i - 1] == ' ') && str[i] != ' ')
+			wordcount++;
+	}
+
+	return (wordcount);
+}
+
 char **strtow(char *str)
 {
 	int i, j, wordcount = 0, k = 0, wordlength = 0, l;
@@ -26,12 +40,7 @@ char **strtow(char *str)
 	if (str[i] == '\0')
 		return (NULL);
 
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if ((i == 0 || str[i - 1] == ' ') && str[i] != ' ')
-			wordcount++;
-	}
+	wordcount = word_count(*str);
 
 	matrix = (char**)malloc((wordcount + 1) * sizeof(char *));
 
