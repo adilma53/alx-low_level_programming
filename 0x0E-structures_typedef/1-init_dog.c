fc79304f0362cce5d5119e07feccc6beee8fb00e
adil mae <itsadilhadid@gmail.com>
2023-04-17 21:51:49 +0000

    alx

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -17,12 +17,17 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d == NULL)
+		return;
+	else
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
+
+
+
 
 }
 
