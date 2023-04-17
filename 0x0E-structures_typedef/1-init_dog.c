06955db9bfa51efd54b5f8435e1c5de0ebf7bcec
adil mae <itsadilhadid@gmail.com>
2023-04-17 20:19:24 +0000

    alx

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -17,9 +19,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
+		d->name = strdup(name);
+		d->age = age;
+		d->owner = strdup(owner);
 	}
 }
 
