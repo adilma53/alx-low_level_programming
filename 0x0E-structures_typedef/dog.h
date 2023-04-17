be037f772f92abd101fe2a53aa5036b6763e3050
adil mae <itsadilhadid@gmail.com>
2023-04-17 18:38:55 +0000

    alx

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,12 +18,6 @@ struct dog
 };
 
 
-/* MY FUNCTIONS PROTOTYPES*/
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-void free_dog(dog_t *d);
-int _strlen(char *str);
-char *_strcopy(char *dest, char *src);
 
 
 
