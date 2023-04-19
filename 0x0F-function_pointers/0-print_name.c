6ca724ed56e8c11c9911de28f94480a9157d9cff
adil mae <itsadilhadid@gmail.com>
2023-04-19 16:32:47 +0000

    alx

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -13,10 +13,12 @@
 void print_name(char *name, void (*f)(char *))
 {
 
-	if (f != NULL || name != NULL)
-	{
-		f(name);
-	}
-	else
+	if (f = NULL || name == NULL)
 		return;
+
+
+
+
+	f(name);
+
 }
