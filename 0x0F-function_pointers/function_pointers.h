921e3e37d30197dbaddc5d0d30a6935f4bc93a96
adil mae <itsadilhadid@gmail.com>
2023-04-19 17:24:07 +0000

    alx

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,7 +2,12 @@
 #define function_pointers
 
 
+/* libraries */
+#include <stdio.h>
+#include <stdlib.h>
 
+
+/* prototypes */
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
