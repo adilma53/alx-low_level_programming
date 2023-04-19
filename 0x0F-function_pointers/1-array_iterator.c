70105964f1dbcc213b106b773b147670b0f879bf
adil mae <itsadilhadid@gmail.com>
2023-04-19 18:28:46 +0000

    alx

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,14 +12,15 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    int i;
+	int i;
 
-    if (array && action)
-    {
-        for (i = 0; i < size; i++)
-        {
-            action(array[i]);
-        }
-    }
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+
 }
 
