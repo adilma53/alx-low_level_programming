c0a540aeecebd139bbac284dc23f74bdc1251cfe
adil mae <itsadilhadid@gmail.com>
2023-04-20 22:21:55 +0000

    alx

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,8 +33,8 @@ void print_all(const char * const format, ...)
 			str = va_arg(args, char *);
 			if (str != NULL)
 				printf("%s%s", separator, str);
-			else
-				printf("%s(nil)", separator);
+
+			printf("%s(nil)", separator);
 			break;
 		default:
 			i++;
