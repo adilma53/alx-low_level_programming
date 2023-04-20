2761a2141c4a4fc891a5e1d4e3e0cfb2ba85b677
adil mae <itsadilhadid@gmail.com>
2023-04-20 19:50:47 +0000

    alx

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
 * print_numbers - a variadic function that prints all its arguments
@@ -9,19 +11,20 @@
 * Return: nothing
 */
 
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-
-	if (n != 0)
+	if (n > 0)
 	{
-
 		unsigned int i;
 		va_list my_arguments;
+		char *my_separator;
 
+		if (separator == NULL || separator[0] == 0)
+			my_separator = "";
+		else
+			my_separator = separator;
 
 		va_start(my_arguments, n);
-
 
 		for (i = 0; i < n; i++)
 		{
@@ -29,11 +32,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 			printf("%d", arg);
 
-			if (separator != NULL && i != n - 1)
-				printf("%s", separator);
-
+			if (i != n - 1)
+				printf("%s", my_separator);
 		}
+
 		va_end(my_arguments);
-		printf("\n");
 	}
+
+	printf("\n");
 }
+
