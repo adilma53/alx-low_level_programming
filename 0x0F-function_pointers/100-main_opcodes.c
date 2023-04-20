83aeb06a1f77ccac0074938b020b895caf6983c0
adil mae <itsadilhadid@gmail.com>
2023-04-20 02:16:50 +0000

    alx

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -4,45 +4,45 @@
 void print_opcodes(char *func_start, int num_bytes);
 
 /**
- * main - entry point
- * @argc: argument count
- * @argv: argument vector
+ * main - _
+ * @argc: arguments
+ * @argv: argument array
  *
- * Return:  0
+ * Return: 0.
  */
 int main(int argc, char **argv)
 {
 	int num_bytes;
 	char *func_start = (char *)main;
-
+	/* check if the number of arguments is correct */
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
+	/* convert the argument to an integer */
 	num_bytes = atoi(argv[1]);
-
+	/* check if the number of bytes is negative */
 	if (num_bytes < 0)
 	{
 		printf("Error\n");
 		return (2);
 	}
-
+	/* print the opcodes of the main function */
 	print_opcodes(func_start, num_bytes);
 
 	return (0);
 }
 
 /**
- * print_opcodes - prints opcodes of a function
- * @func_start: starting address of the function
- * @num_bytes: number of bytes to print
+ * print_opcodes - prints opcodes
+ * @func_start: address of the function
+ * @num_bytes: number of bytes
  */
 void print_opcodes(char *func_start, int num_bytes)
 {
 	int i;
-
+	/* print the opcodes in hexadecimal format, separated by spaces */
 	for (i = 0; i < num_bytes; i++)
 	{
 		printf("%.2hhx", func_start[i]);
