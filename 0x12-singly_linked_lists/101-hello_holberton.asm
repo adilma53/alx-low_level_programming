e7943e678cc2e07b54e58c02a7fad89aa0715dab
adil mae <itsadilhadid@gmail.com>
2023-04-28 01:46:52 +0100

    alx

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+section .text
+    global main
+    extern printf
+
+main:
+    push message
+    push format
+    call printf
+    add rsp, 16
+    ret
+
+section .data
+    message db "Hello, Holberton", 0
+    format db "%s\n", 0
