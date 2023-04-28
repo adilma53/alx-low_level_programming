336eaa4daedb4fb3045cfc046cbe35c7e0015b7f
adil mae <itsadilhadid@gmail.com>
2023-04-28 01:36:46 +0100

    alx

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * faster_ - prints a message before the main.
+ */
+
+void __attribute__((constructor)) faster_()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
