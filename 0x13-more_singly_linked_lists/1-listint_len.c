53b803dd424fa7390a8aa1b043fe96dc7ca3d17a
adil mae <itsadilhadid@gmail.com>
2023-05-01 20:45:13 +0100

    alx

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of linked list
+ * listint_len - list length
  * @h: pointer to the head of the linked list
  *
  * Return: number of nodes in the list
  */
-size_t print_listint(const listint_t *h)
+
+size_t listint_len(const listint_t *h)
 {
 	size_t counter = 0;
 
@@ -16,32 +17,4 @@ size_t print_listint(const listint_t *h)
 		counter++;
 	}
 	return (counter);
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	listint_t *head;
-	listint_t *new;
-	listint_t hello = {8, NULL};
-	size_t n;
-
-	head = &hello;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	new->n = 9;
-	new->next = head;
-	head = new;
-	n = listint_len(head);
-	printf("-> %lu elements\n", n);
-	free(new);
-	return (0);
 }
