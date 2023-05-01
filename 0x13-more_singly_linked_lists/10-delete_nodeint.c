22783adf20bfef07ea271076a45a902edab213b5
adil mae <itsadilhadid@gmail.com>
2023-05-01 20:18:12 +0100

    alx

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,33 +9,33 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *temporary_node;
-    listint_t *node_before;
-    unsigned int i;
+	listint_t *temporary_node;
+	listint_t *node_before;
+	unsigned int i;
 
-    if (!head)
-        return (-1);
+	if (!head)
+		return (-1);
 
-    if (index == 0)
-    {
-        temporary_node = *head;
-        *head = (*head)->next;
-        free(temporary_node);
-        return (1);
-    }
+	if (index == 0)
+	{
+		temporary_node = *head;
+		*head = (*head)->next;
+		free(temporary_node);
+		return (1);
+	}
 
-    node_before = *head;
-    for (i = 0; i < index - 1 && node_before; i++)
-    {
-        node_before = node_before->next;
+	node_before = *head;
+	for (i = 0; i < index - 1 && node_before; i++)
+	{
+		node_before = node_before->next;
 
-        if (node_before == NULL)
-            return (-1);
-    }
+		if (node_before == NULL)
+			return (-1);
+	}
 
-    temporary_node = node_before->next;
-    node_before->next = temporary_node->next;
-    free(temporary_node);
+	temporary_node = node_before->next;
+	node_before->next = temporary_node->next;
+	free(temporary_node);
 
-    return (1);
+	return (1);
 }
