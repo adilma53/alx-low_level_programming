9de9a3ffd2621be79f85a028b470f4396e2010d2
adil mae <itsadilhadid@gmail.com>
2023-05-02 01:30:19 +0100

    alx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,30 +11,30 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *before_node;
-    listint_t *new_node;
-    unsigned int i;
+	listint_t *before_node;
+	listint_t *new_node;
+	unsigned int i;
 
-    if (!head)
-        return NULL;
+	if (!head)
+		return (NULL);
 
-    if (idx == 0)
-        return (add_nodeint(head, n));
+	if (idx == 0)
+		return (add_nodeint(head, n));
 
-    before_node = *head;
-    for (i = 0; i < idx - 1 && before_node; i++)
-        before_node = before_node->next;
+	before_node = *head;
+	for (i = 0; i < idx - 1 && before_node; i++)
+		before_node = before_node->next;
 
-    if (!before_node)
-        return (NULL);
+	if (!before_node)
+		return (NULL);
 
-    new_node = malloc(sizeof(listint_t));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->next = before_node->next;
-    before_node->next = new_node;
+	new_node->n = n;
+	new_node->next = before_node->next;
+	before_node->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
