90b3636c0854f257acf21878a56e1d41163618c4
adil mae <itsadilhadid@gmail.com>
2023-05-04 20:23:43 +0100

    alx

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
- * _putchar - writes a character to stdout.
- *
- * @c: The character to print.
- *
- * Return: On success 1. On error, -1 is returned.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * print_binary - prints the binary representation of a number
  * @n: the number to be printed in binary
  *
