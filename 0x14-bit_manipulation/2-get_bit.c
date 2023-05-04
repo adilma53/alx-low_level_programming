2754672d64609cc2092533839036d0eb06a9d627
adil mae <itsadilhadid@gmail.com>
2023-05-04 20:40:46 +0100

    alx

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,12 +10,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index >= (sizeof(unsigned long int) * 8))
-    {
-        return (-1);
-    }
-    else
-    {
-        return ((n >> index) & 1);
-    }
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+	else
+	{
+		return ((n >> index) & 1);
+	}
 }
