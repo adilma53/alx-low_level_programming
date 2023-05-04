83bdcc191473dfcb9e270ab911e11d65350b1f1d
adil mae <itsadilhadid@gmail.com>
2023-05-04 19:53:26 +0100

    alx

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,22 +8,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int number = 0;
-    unsigned int i = 0;
+	unsigned int number = 0;
+	unsigned int i = 0;
 
-    if (!b)
-        return (0);
+	if (!b)
+		return (0);
 
-    for (; b[i] != '\0'; i++)
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return (0);
+	for (; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 
-        number = number << 1;
+		number = number << 1;
 
-        if (b[i] == '1')
-            number += 1;
-    }
+		if (b[i] == '1')
+			number += 1;
+	}
 
-    return (number);
+	return (number);
 }
