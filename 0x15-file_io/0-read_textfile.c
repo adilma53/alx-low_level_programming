5659da1b464a270ee8e99887f48170f8e135976e
adil mae <itsadilhadid@gmail.com>
2023-05-09 00:25:34 +0100

    alx

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,20 +20,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * (letters + 1));
-
 	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-
 	bytes_read = read(fd, buffer, letters);
-
 	if (bytes_read == -1)
 	{
 		close(fd);
@@ -42,9 +38,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	buffer[bytes_read] = '\0';
-
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
 		close(fd);
