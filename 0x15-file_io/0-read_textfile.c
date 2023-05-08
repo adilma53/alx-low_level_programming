b928211a262b9c57bc2ac61c1f76a46e6f1793c0
adil mae <itsadilhadid@gmail.com>
2023-05-09 00:23:38 +0100

    alx

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,39 +11,43 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor;
-	ssize_t _read, _written;
-	char *_buffer;
-
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
+	int fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
 
-	_buffer = malloc(sizeof(char) * (letters + 1));
-	if (!_buffer)
-		return (0);
+	char *buffer = malloc(sizeof(char) * (letters + 1));
 
-	_read = read(file_descriptor, _buffer, letters);
-	if (_read == -1)
+	if (buffer == NULL)
 	{
-		free(_buffer);
-		close(file_descriptor);
-		return (0);
-	}
-	_buffer[_read] = '\0';
-
-	_written = write(STDOUT_FILENO, _buffer, _read);
-	if (_written == -1 || _written != _read)
-	{
-		free(_buffer);
-		close(file_descriptor);
+		close(fd);
 		return (0);
 	}
 
-	free(_buffer);
-	close(file_descriptor);
-	return (_written);
+	ssize_t bytes_read = read(fd, buffer, letters);
+
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	buffer[bytes_read] = '\0';
+
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	close(fd);
+	free(buffer);
+	return (bytes_written);
 }
