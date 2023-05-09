de88c8d07b3ccab9a439ae813ea370e6d6419f45
adil mae <itsadilhadid@gmail.com>
2023-05-09 01:23:31 +0100

    alx

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,39 +2,41 @@
 
 /* by adilma53 */
 /**
- * append_text_to_file - appends text to the end file
+ * create_file - appends text to the end file
  *
- * @filename: file to append in
- * @text_content: string to append
+ * @filename: file to create
+ * @text_content: its content
  *
  * Return: 1 success or 0 for F
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	int file_descripto, len = 0, _written;
+	int file_descriptor, length = 0, _written;
+	mode_t mode = O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR;
 
 	if (!filename)
 		return (-1);
 
-	if (text_content)
-		while (text_content[len++] != '\0')
-			;
-
-	file_descripto = open(filename, O_WRONLY | O_APPEND);
-	if (file_descripto == -1)
+	file_descriptor = open(filename, mode);
+	if (file_descriptor == -1)
 		return (-1);
 
 	if (text_content != NULL)
-		_written = write(file_descripto, text_content, len);
-
-	if (_written == -1)
 	{
-		close(file_descripto);
-		return (-1);
+		while (text_content[length])
+			length++;
+
+		_written = write(file_descriptor, text_content, length);
+
+		if (_written != length)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
 	}
 	else
 	{
-		close(file_descripto);
+		close(file_descriptor);
 		return (1);
 	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /* my prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
