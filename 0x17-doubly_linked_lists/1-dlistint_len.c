a7402640af490d1b398e1ef59c5b511533231a87
adil majhoul <itsadilhadid@gmail.com>
2023-06-16 00:24:22 +0100

    ALX HIGH LEVEL

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - get list length
+ * @h: node to start with
+ * Return: number of nodes
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+	h = h->prev;
+	i++;
+	}
+
+	return (i);
+}
