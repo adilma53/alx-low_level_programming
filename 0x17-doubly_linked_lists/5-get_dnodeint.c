72636934802f8407aaad86f89f6869e06ced4834
adil majhoul <itsadilhadid@gmail.com>
2023-06-16 02:51:02 +0100

    ALX HIGH LEVEL

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - get list at index
+ * @head: node to start with
+ * @index: index tp get
+ * Return: node at index
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int i = 0;
+
+	while (i <= index)
+	{
+	head = head->next;
+	i++;
+	}
+	return (head);
+}
