237a92a3970b2ca5c9cb5b5316340e7caae86613
adil majhoul <itsadilhadid@gmail.com>
2023-06-17 02:08:06 +0100

    ALX HIGH LEVEL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -6,19 +6,17 @@
  * @n: node data
  * Return: node added
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	int i = 0;
-	int j = 0;
+	unsigned int i = 0;
+	unsigned int j = 0;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
-	new_node->n = n;
 	dlistint_t *head_copy1 = *h;
 	dlistint_t *head_copy2 = *h;
 	dlistint_t *left_node;
 	dlistint_t *right_node;
 
+	new_node->n = n;
 	while (head_copy1)
 	{
 		head_copy1 = head_copy1->next;
@@ -45,7 +43,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			/* coneect right node to new node*/
 			(right_node)->prev = new_node;
 
-			return (new_node);
+			break;
 		}
 	}
+	return (new_node);
+}
+
+int main(void)
+{
+	dlistint_t *head;
+
+	head = NULL;
+	add_dnodeint_end(&head, 0);
+	add_dnodeint_end(&head, 1);
+	add_dnodeint_end(&head, 2);
+	add_dnodeint_end(&head, 3);
+	add_dnodeint_end(&head, 4);
+	add_dnodeint_end(&head, 98);
+	add_dnodeint_end(&head, 402);
+	add_dnodeint_end(&head, 1024);
+	print_dlistint(head);
+	printf("-----------------\n");
+	insert_dnodeint_at_index(&head, 5, 4096);
+	print_dlistint(head);
+	free_dlistint(head);
+	head = NULL;
+	return (EXIT_SUCCESS);
 }
