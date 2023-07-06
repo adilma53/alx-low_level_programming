64943b232d9e4fcf065c0535d0964bbd5185b174
adil majhoul <itsadilhadid@gmail.com>
2023-07-06 16:08:17 +0100

    ALX HIGH LEVEL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	size_t i;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (!table)
@@ -19,6 +20,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
