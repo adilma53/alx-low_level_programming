d3bd88eb869d068dae46842843d2595a50ad3979
adil mae <itsadilhadid@gmail.com>
2023-10-09 14:33:24 +0100

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search for a value in array of integers
+ * @array: array to search
+ * @size: size of array
+ * @value: the value to search for
+ *
+ * Return: return first accurence of target or -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
