17ca1e34bbd188767890b29282cb8915b70556e6
adil mae <itsadilhadid@gmail.com>
2023-10-12 00:48:47 +0100

    task 1

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,1 +1,41 @@
 #include "search_algos.h"
+
+ /**
+ * jump_list - search for value in array
+ * using the interpolation search algorithm
+ * @array: array to search in
+ * @size: size of array
+ * @value: value to search for
+ *
+ * Return: first index where value is located
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t step, step_size;
+	listint_t *node, *jump;
+
+	if (!list || size == 0)
+		return (NULL);
+
+	step = 0;
+	step_size = sqrt(size);
+
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
+	{
+		node = jump;
+		for (step += step_size; jump->index < step; jump = jump->next)
+		{
+			if (jump->index + 1 == size)
+				break;
+		}
+		printf("Value checked array[%ld] = [%d]\n", jump->index, jump->n);
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n",
+		   node->index, jump->index);
+
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
+}
