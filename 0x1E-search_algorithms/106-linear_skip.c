981462c096a31a6e1bd9babadbb309b266e6285b
adil mae <itsadilhadid@gmail.com>
2023-10-12 01:17:58 +0100

    task 1

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,41 +1,40 @@
 #include "search_algos.h"
 /**
- * linear_search - binary search algorithm function
+ * linear_skip - binary search algorithm function
  * @list: list to search
  * @value: the value to search for
  *
  * Return: return first accurence of target
  */
-skiplist_t *linear_search(skiplist_t *list, int value)
+skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *node, *jump;
+	skiplist_t *node, *step;
 
-	if (!list)
+	if (list == NULL)
 		return (NULL);
 
-	for (node = jump = list; jump->next && jump->next != NULL && jump->n < value;)
+	for (node = step = list; step->next != NULL && step->n < value;)
 	{
-		node = jump;
-
-		if (jump->express != NULL)
+		node = step;
+		if (step->express != NULL)
 		{
-			jump = jump->express;
-			printf("Value checked array[%ld] = [%d]\n",
-				   jump->index, jump->n);
+			step = step->express;
+			printf("Value checked at index [%ld] = [%d]\n",
+					step->index, step->n);
 		}
 		else
 		{
-			while (jump->next != NULL)
-			{
-				jump = jump->next;
-			}
+			while (step->next != NULL)
+				step = step->next;
 		}
 	}
+
 	printf("Value found between indexes [%ld] and [%ld]\n",
-		   node->index, jump->index);
-	for (; node->index < jump->index && node->n < value; node = node->next)
-		printf("Value checked array[%ld] = [%d]\n", node->index, node->n);
-	printf("Value checked array[%ld] = [%d]\n", node->index, node->n);
+			node->index, step->index);
+
+	for (; node->index < step->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
 	return (node->n == value ? node : NULL);
 }
