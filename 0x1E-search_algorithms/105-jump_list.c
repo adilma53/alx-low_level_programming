312876f1178985f24410ef46e32547082112acb2
adil mae <itsadilhadid@gmail.com>
2023-10-12 01:15:54 +0100

    task 1

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-
 /**
  * jump_list - search for value in list uses jump search algorithm
  * @list: list to search in
@@ -8,30 +7,29 @@
  *
  * Return: return first accurence of target
  */
-
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t step, step_size;
+	size_t step, stepSize;
 	listint_t *node, *jump;
 
-	if (!list || size == 0)
+	if (list == NULL || size == 0)
 		return (NULL);
 
 	step = 0;
-	step_size = sqrt(size);
-
+	stepSize = sqrt(size);
 	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
 	{
 		node = jump;
-		for (step += step_size; jump->index < step; jump = jump->next)
+		for (step += stepSize; jump->index < step; jump = jump->next)
 		{
 			if (jump->index + 1 == size)
 				break;
 		}
-		printf("Value checked array[%ld] = [%d]\n", jump->index, jump->n);
+		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
 	}
+
 	printf("Value found between indexes [%ld] and [%ld]\n",
-		   node->index, jump->index);
+			node->index, jump->index);
 
 	for (; node->index < jump->index && node->n < value; node = node->next)
 		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
